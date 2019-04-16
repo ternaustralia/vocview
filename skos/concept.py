f88463abd3eaa2f05feefdd3b5dc81fff4b588c7
Edmond Chuc <edmond.chuc@outlook.com>
2019-04-16 10:06:42 +1000

    Separated common properties in Concept class. Removed some subClass property for SKOS notes.

--- skos/concept.py
+++ skos/concept.py
@@ -1,15 +1,10 @@
 import skos
+from skos.common_properties import CommonPropertiesMixin
 
 
-class Concept:
+class Concept(CommonPropertiesMixin):
     def __init__(self, uri):
-        self.uri = uri
-        self.label = skos.get_label(uri)
-        self.class_types = skos.get_class_types(uri)
-        self.alt_labels = skos.get_alt_labels(uri)
-        self.definition = skos.get_definition(uri)
-        self.created = skos.get_created_date(uri)
+        super().__init__(uri)
         self.narrowers = skos.get_narrowers(uri)
         self.broaders = skos.get_broaders(uri)
         self.top_concept_of = skos.get_top_concept_of(uri)
-        self.properties = skos.get_properties(uri)
