cdb8e7d02878c84dcd922ccfae14b359a0c3b6c1
Edmond Chuc <37032744+edmondchuc@users.noreply.github.com>
2019-07-15 15:19:42 +1000

    Updated routes to support pagination.

--- controller/routes.py
+++ controller/routes.py
@@ -68,17 +68,27 @@ def index():
 
 @routes.route('/vocabulary/', methods=['GET'])
 def render_vocabulary_register():
+    page = request.values.get('page')
+    if page is None:
+        page = 1
 
     items = skos.list_concept_schemes()
 
     query = request.values.get('search')
     items = process_search(query, items)
+    
+    total_items_count = len(items)
+    page_from = int(page)
+    page_size = 20
+
+    items = items[(page_from - 1) * page_size:page_size * page_from]
 
     items = munchify(items)
 
     r = skos.Register(request, 'Register of SKOS vocabularies',
                       'This register contains a listing of SKOS vocabularies as concept schemes or collections.',
                       items, ['http://www.w3.org/2004/02/skos/core#ConceptScheme'],
+                      total_items_count=total_items_count,
                       register_template='register.html',
                       title='Vocabularies',
                       description='Register of all vocabularies in this system.',
@@ -88,11 +98,20 @@ def render_vocabulary_register():
 
 @routes.route('/concept/', methods=['GET'])
 def render_concept_register():
+    page = request.values.get('page')
+    if page is None:
+        page = 1
 
     items = skos.list_concepts()
 
     query = request.values.get('search')
     items = process_search(query, items)
+    
+    total_items_count = len(items)
+    page_from = int(page)
+    page_size = 20
+
+    items = items[(page_from - 1) * page_size:page_size * page_from]
 
     items = munchify(items)
 
@@ -100,6 +119,7 @@ def render_concept_register():
                       'Register of SKOS concepts',
                       'This register contains a listing of all SKOS concepts within this system.',
                       items, ['http://www.w3.org/2004/02/skos/core#Concept'],
+                      total_items_count=total_items_count,
                       register_template='register.html',
                       title='Concepts',
                       description='Register of all vocabulary concepts in this system.',
